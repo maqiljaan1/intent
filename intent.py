8a2666d652034e8e8fc9afc2a255350c5a87afbe
Marvin M. Rosales <maqiljaanrosales@outlook.com>
2020-09-15 21:09:24 +0800

    post several test

--- intent.py
+++ intent.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 
 vocab_size = 10000
 embedding_dim = 16
@@ -10,6 +12,7 @@ trunc_type = 'post'
 padding_type = 'post'
 oov_tok = "<OOV>"
 training_size = 20000
+num_epochs = 500
 
 with open("dataset//email_response.json","r") as f:
     datastore = json.load(f)
@@ -40,7 +43,6 @@ training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=p
 testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
 testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
 
-import numpy as np
 
 training_padded = np.array(training_padded)
 training_labels = np.array(training_labels)
@@ -58,21 +60,19 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 model.summary()
 
-num_epochs = 500
 history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
 
-import matplotlib.pyplot as plt
 
-def plot_graphs(history, string):
-    plt.plot(history.history[string])
-    plt.plot(history.history['val_'+string])
-    plt.xlabel("Epochs")
-    plt.ylabel(string)
-    plt.legend([string, 'val_'+string])
-    plt.show()
+# def plot_graphs(history, string):
+    # plt.plot(history.history[string])
+    # plt.plot(history.history['val_'+string])
+    # plt.xlabel("Epochs")
+    # plt.ylabel(string)
+    # plt.legend([string, 'val_'+string])
+    # plt.show()
 
-plot_graphs(history, "accuracy")
-plot_graphs(history, "loss")
+# plot_graphs(history, "accuracy")
+# plot_graphs(history, "loss")
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
@@ -83,30 +83,37 @@ print(decode_sentence(training_padded[0]))
 print(training_sentences[2])
 print(labels[2])
 
-
-
 model.save('saved_model/my_model')
 
 e = model.layers[0]
 weights = e.get_weights()[0]
-# print(weights.shape)
 
+# sentence = ["nah, let's do it next week", "please cancel this invitation", "maybe we should move it", "that sounds great"]
+# sequences = tokenizer.texts_to_sequences(sentence)
+# padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
 
-#running on tensor flow
-#500 epochs dummy data
+# results = model.predict(padded)
 
+def get_new_sentence(input_sentence):
+    sentence = [input_sentence]
+    sequences = tokenizer.texts_to_sequences(sentence)
+    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    results = model.predict(padded)
+    i = 0
+    for res in results:
+        if res[0] > 0.5:
+            print('"', sentence[i],'" - is a ','confirmation email ', res[0], sep='')
+        else:
+            print('"', sentence[i],'" - is a ','non-confirmation email ', res[0], sep='')
+        i+=1
 
-sentence = ["nah, let's do it next week", "please cancel this invitation", "maybe we should move it", "that sounds great"]
-sequences = tokenizer.texts_to_sequences(sentence)
-padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+exit_phrase = 'quit'
+stop_now = False
 
-results = model.predict(padded)
-ii = 0
-for res in results:
-    print('"',sentence[ii],'"')
-    if res[0] > 0.5:
-        print('confirmation email ', res[0])
-    else:
-        print('non-confirmation email ', res)
-    ii+=1
-    print('\n')
+print('\n\n\nPlease enter a phrase below\n\n')
+
+while not stop_now:
+    user_input = input()
+    if user_input == 'quit':
+        stop_now = True
+    get_new_sentence(user_input)
