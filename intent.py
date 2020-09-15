09dc9e24e0e53d1342b4f8dadb2c1c49d8576e77
Marvin M. Rosales <maqiljaanrosales@outlook.com>
2020-09-15 21:13:29 +0800

    before push

--- intent.py
+++ intent.py
@@ -62,18 +62,6 @@ model.summary()
 
 history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
 
-
-# def plot_graphs(history, string):
-    # plt.plot(history.history[string])
-    # plt.plot(history.history['val_'+string])
-    # plt.xlabel("Epochs")
-    # plt.ylabel(string)
-    # plt.legend([string, 'val_'+string])
-    # plt.show()
-
-# plot_graphs(history, "accuracy")
-# plot_graphs(history, "loss")
-
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 def decode_sentence(text):
@@ -87,12 +75,6 @@ model.save('saved_model/my_model')
 
 e = model.layers[0]
 weights = e.get_weights()[0]
-
-# sentence = ["nah, let's do it next week", "please cancel this invitation", "maybe we should move it", "that sounds great"]
-# sequences = tokenizer.texts_to_sequences(sentence)
-# padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-
-# results = model.predict(padded)
 
 def get_new_sentence(input_sentence):
     sentence = [input_sentence]
